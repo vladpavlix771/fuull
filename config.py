7ff485807640f6a44be97683ed8d1d8ea0ea225b
vladpavlix771 <56918679+vladpavlix771@users.noreply.github.com>
2022-04-15 19:30:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#Токен вашего бота
+bot_token = '5152386094:AAEfiLRK-pkEm_MlDYdwqdMLZJ9cgW9hkvo'
+#ID админа
+admin = 1370967532
+#Юзер саппорта
+support = '@blackToT_support'
+#Канал с отзывами
+#feedback = 'https://t.me/Black_ToT_feedback'
+#Название магазина
+shop_name = 'BlackToT'
